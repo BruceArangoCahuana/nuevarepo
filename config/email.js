07cffc7b54e2dd2272dee77740236380cf65e0d9
BruceArangoCahuana <bruce_cahuana@hotmail.com>
2022-04-03 00:19:03 -0500

    actualiazado mails

--- config/email.js
+++ config/email.js
@@ -16,13 +16,14 @@ const sendMail  = async(user)=>{
     const transporter = createTransport();
     const info = await transporter.sendMail({
         from:"Top Tour Peru<toptoursperu@gmail.com>",
-        to:user.correo,
+        to:"toptoursperu@gmail.com",
         subject:`Consulta de usuario${user.nombre}`,
         html:`<div style='background:"red";width:"100%";height:"300px"'>
             <h3>Top Tour Peru</h3>
         <div>
         <div>
             <h4>usuario:${user.nombre}</h4>
+            <h4>Correo:${user.correo}</h4>
             <h4>consulta:${user.consulta}</h4>
         <div>`
     })
